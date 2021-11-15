0f1c26ca37b7808174b162ffc88556531c8b528a
Jeny.Macu <33354584+JenyMacu@users.noreply.github.com>
2021-11-15 00:15:11 -0600

    adad

--- Catedraticos/Controllers/CursoControllerAPI.cs
+++ Catedraticos/Controllers/CursoControllerAPI.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Catedraticos.Controllers
+namespace Curso.Controllers
 {
     [Route("api/Curso")]
     [ApiController]
